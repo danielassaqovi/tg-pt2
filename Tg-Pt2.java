0c4295e0ca7439145b735691db4a0d902a10421a
m-fatahillah <fatahcrown789@gmail.com>
2024-09-05 16:22:01 +0700

    p

--- Tg-Pt2.java
+++ Tg-Pt2.java
@@ -2,11 +2,14 @@ public class Tg-Pt2 {
   public static void main(String[] args) {
       System.out.println("Nama Kelompok");
 
-       System.out.println("Nama : MOCHAMMAD FAISOL BISMAGRAHA");
-       System.out.println("NIM  : 244107020010");
+      System.out.println("Nama : MOCHAMMAD FAISOL BISMAGRAHA");
+      System.out.println("NIM  : 244107020010");
 
       System.out.println("Nama : Muhammad Daniel Assaqovi");
-      System.out.println("Nim  :244107020061");
+      System.out.println("Nim  : 244107020061");
+
+      System.out.println("Nama : Muhammad Fatahillah Athabrani");
+      System.out.println("Nim  : 244107020121");
 
 
   }
