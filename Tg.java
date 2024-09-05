f2d3a90c19e18ebdb1654a15ee11c4dc6ee2512f
m-fatahillah <fatahcrown789@gmail.com>
2024-09-05 16:12:00 +0700

    1

---
+++ Tg.java
@@ -0,0 +1,7 @@
+public class Tg-Pt2 {
+  public static void main(String[] args) {
+      System.out.println("Nama Kelompok");
+      System.out.println("Nama : Muhammad Fatahillah Athabrani");
+      System.out.println("NIM  : 244107020121");
+  }
+}
