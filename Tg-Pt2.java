80a6a24bfeb79d6a46b01ae7efca96082aba33a0
damielassaqovi <palurejodaniel@gail.com>
2024-09-05 16:07:00 +0700

    new file:   Tg-Pt2.java

---
+++ Tg-Pt2.java
@@ -0,0 +1,7 @@
+public class Tg-Pt2 {
+  public static void main(String[] args) {
+      System.out.println("Nama Kelompok");
+      System.out.println("Nama : Muhammad Daniel Assaqovi");
+      System.out.println("Nim  :244107020061");
+  }
+}
