3727f073dc1fe62fcf9c6b84b244af1fb5996a29
faisolbisma18 <bismagraha2006@gmail.com>
2024-09-05 16:11:22 +0700

    modified:   Tg-Pt2.java

--- Tg-Pt2.java
+++ Tg-Pt2.java
@@ -1,5 +1,7 @@
 public class Tg-Pt2 {
   public static void main(String[] args) {
       System.out.println("Nama Kelompok");
+       System.out.println("Nama : MOCHAMMAD FAISOL BISMAGRAHA");
+       System.out.println("NIM  : 244107020010");
   }
 }
